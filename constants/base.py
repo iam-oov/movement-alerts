3580d3be13388094707d2b3a7f6b912b0eb2bfc8
osvaldo hinojosa <osvaldo.javier14@gmail.com>
2025-03-30 00:26:09 -0600

    fix: constants file

--- constants/base.py
+++ constants/base.py
@@ -1,9 +1,9 @@
 # Trading parameters
 TRADING = {
-    'STOP_LOSS_PERCENTAGE': 3.50,  # Stop loss percentage per operation
-    'TAKE_PROFIT_PERCENTAGE': 3.50,  # Take profit percentage per operation
+    'STOP_LOSS_PERCENTAGE': 3.5,  # Stop loss percentage per operation
+    'TAKE_PROFIT_PERCENTAGE': 3.5,  # Take profit percentage per operation
     'VARIATION_PERCENTAGE': 5,  # variation to activate the operation of possible pairs
-    'VARIATION_100K_PERCENTAGE': 7,  # variation for pairs with volume less than 100k
+    'VARIATION_100K_PERCENTAGE': 6,  # variation for pairs with volume less than 100k
     'VARIATION_FAST_PERCENTAGE': 2,  # variation for rapid upward movements
 }
 
@@ -19,10 +19,22 @@ SOUND = {
 NOTIFICATIONS = {
     'ACTIVE': True,
 }
+WIN = {
+    'name': 'WIN',
+    'emoji': '🟢'
+}
+LOSE = {
+    'name': 'LOSE',
+    'emoji': '🔴'
+}
+IN_PROGRESS = {
+    'name': 'IN_PROGRESS',
+    'emoji': '🟡'
+}
 
 # Operation types
 FAST_SHORT = {
-    'name': 'FAST SHORT',
+    'name': 'FAST_SHORT',
     'emoji': '🔴🔥'
 }
 LONG = {
